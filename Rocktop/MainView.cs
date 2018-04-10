dbc08464c8781f2ca252854e48f3be347454bd8a
Scott Brown <scottbrownr@gmail.com>
2018-04-09 20:28:22 -0400

    page2

--- Rocktop/MainView.cs
+++ Rocktop/MainView.cs
@@ -20,12 +20,14 @@ namespace Rocktop
             View.BackgroundColor = UIColor.White;
             var title = new UILabel();
             title.Text = "Rocktop";
-            title.Font = UIFont.PreferredHeadline;
-            title.Frame = new CoreGraphics.CGRect(30, 50, View.Bounds.Width - 60, 50);
+            title.Font = UIFont.FromName("Helvetica-Bold", 60f);
+            title.Frame = new CoreGraphics.CGRect(30, 70, 200, 50);
+            title.TextAlignment = UITextAlignment.Center;
             var counter = new UILabel();
             counter.Text = "$3,405,691";
-            counter.Font = UIFont.PreferredTitle2;
-            counter.Frame = new CoreGraphics.CGRect(30, 130, View.Bounds.Width - 60, 40);
+            counter.Font = UIFont.FromName("Helvetica", 70f);
+            counter.Frame = new CoreGraphics.CGRect(30, 130, 200, 80);
+            counter.TextAlignment = UITextAlignment.Center;
             View.AddSubview(title);
             View.AddSubview(counter);
         }
