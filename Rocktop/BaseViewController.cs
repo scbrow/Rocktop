b9e1710f9393d9a18d42377f0ab20e76dae03366
Scott Brown <scottbrownr@gmail.com>
2018-04-25 22:54:54 -0400

    .dll

--- Rocktop/BaseViewController.cs
+++ Rocktop/BaseViewController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using garbage;
 using Foundation;
 using UIKit;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+using System.Reflection;
 
 namespace Rocktop
 {
@@ -39,11 +43,33 @@ namespace Rocktop
             loginButton.BackgroundColor = UIColor.White;
             loginButton.TouchUpInside += delegate
             {
-                NavigationController.PushViewController(next, true);
+                if(connect("login," + usernameField.Text + "," + passwordField.Text))
+                    NavigationController.PushViewController(next, true);
+            };
+            var createButton = UIButton.FromType(UIButtonType.RoundedRect);
+            createButton.Frame = new CoreGraphics.CGRect(30, View.Bounds.Height / 3 + 130, (View.Bounds.Width - 60), 40);
+            createButton.SetTitle("Submit", UIControlState.Normal);
+            createButton.Font = UIFont.PreferredSubheadline;
+            createButton.BackgroundColor = UIColor.White;
+            createButton.TouchUpInside += delegate
+            {
+                if (connect("create," + usernameField.Text + "," + passwordField.Text))
+                    NavigationController.PushViewController(next, true);
             };
             View.AddSubview(usernameField);
             View.AddSubview(passwordField);
             View.AddSubview(loginButton);
+            View.AddSubview(createButton);
+        }
+        public bool connect(string input)
+        {
+            java.lang.Class clie = typeof(Garbo);
+            java.lang.Thread.currentThread().setContextClassLoader(clie.getClassLoader());
+            Garbo g = new Garbo();
+            int output = g.connect(input);
+            if (output == 1 || output == 2)
+                return true;
+            return false;
         }
     }
 }
